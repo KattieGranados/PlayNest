bbba0f06d97b2aaf6366af6bcc0c35725a2e09c5
Kattie Granados <kattieadriana@gmail.com>
2023-12-15 19:29:38 -0500

    testing something

--- tests/e2e/test_all_users.py
+++
@@ -1,6 +0,0 @@
-from flask.testing import FlaskClient
-
-def test_all_users(test_app: FlaskClient):
-  # Test that the page loads
-  response = test_app.get('/users')
-  assert response.status_code == 200
